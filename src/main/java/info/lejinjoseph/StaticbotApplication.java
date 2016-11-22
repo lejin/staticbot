9e09627ada2dbe046ea61ea72678fcade29908d2
lejin <lejinjoseph@outlook.com>
2016-11-23 00:06:47 +0530

    initial commit

---
+++ src/main/java/info/lejinjoseph/StaticbotApplication.java
@@ -0,0 +1,12 @@
+package info.lejinjoseph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StaticbotApplication.class, args);
+	}
+}
